03a46e751e4dd153779bca9c33a5943a360d8d8f
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2024-10-31 16:53:05 -0600

    create accounts app, SpaceTraveler user

---
+++ accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+
+# urlpatterns = [
+# 	path('signup/', SignUpView.as_view(), name='signup'),
+# ]
--- django_project/urls.py
+++ django_project/urls.py
@@ -20,6 +20,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	# search our auth stuff first, then pass the rest off to the base libs
+	path('accounts/', include('accounts.urls')),
+	path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
