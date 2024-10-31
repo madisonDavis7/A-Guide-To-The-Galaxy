18e641c24c39fa401a96b3298a9e4a656c2a3240
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2024-10-31 11:39:21 -0600

    add required django apps and settings for deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+FORCE_SCRIPT_NAME = '/' + os.environ.get('SITE_NAME', '')
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,22 +27,40 @@ SECRET_KEY = 'django-insecure-4$6@5&r4%kex2%me935-8q^=ep=ufnyv89&i7@dx^68924o2q#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	'localhost',
+	'127.0.0.1',
+	'csci258.cs.umt.edu',  # this is the url of the VM
+]
+INTERNAL_IPS = [
+	'127.0.0.1',
+	'localhost',
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+	# --  Built-ins -- #
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	
+	# -- 3rd-party apps -- #
+	'debug_toolbar',
+	'crispy_forms',
+	# 'crispy_bootstrap5',
+	# 'django_extensions',
+	
+    # -- Local apps -- #
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,16 +94,33 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+	# PostgreSQL database used in production
+    'prod': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': os.environ.get('POSTGRES_DB'),
+		'USER': os.environ.get('POSTGRES_USER'),
+		'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+		'HOST': 'postgres',
+		'PORT': '5432',
+	},
+
+	# SQLite3 database used for development and testing
+	'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+# defaults to local if not set in environment variable
+default_database = os.environ.get('DJANGO_DATABASE', 'local')
+# sets detected database to default
+DATABASES['default'] = DATABASES[default_database]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
+# AUTH_USER_MODEL = 'accounts.CustomUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -100,22 +137,36 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+LOGIN_URL = FORCE_SCRIPT_NAME + '/accounts/login'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
 USE_I18N = True
 
+TIME_ZONE = 'America/Denver'
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# URL path to serve static files from; ex: '/group-3/static/'
+STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
+# project static files location
+STATICFILES_DIRS = [ BASE_DIR / "static" ]
+# collected static files location; includes other apps, like admin
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# enable caching and compression when serving static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
