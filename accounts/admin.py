2ea37bcd655b40cfa475b07a555c0c266e904b80
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2024-11-03 12:34:13 -0700

    register custom user in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import SpaceTraveler
+from .forms import SpaceTravelerCreationForm, SpaceTravelerChangeForm
+
+
+@admin.register(SpaceTraveler)
+class TutorUserAdmin(UserAdmin):
+	# creation
+	add_form = SpaceTravelerCreationForm
+	# modification
+	form = SpaceTravelerChangeForm
+
+	# model = SpaceTraveler
+	# fields displayed in the user list
+	list_display = [
+		'first_name',
+		'last_name',
+		'email', 
+		'home_planet',
+		'is_staff',
+	]
+	
+	# fields displayed during creation
+	add_fieldsets = (
+		(
+			None,
+			{
+				"fields": (
+					"username", 
+					# "usable_password", 
+					"password1", "password2",
+					'security_question', 'security_answer',
+				)
+			}
+		),
+		(
+			_("Personal info"), 
+			{ "fields": (
+				"first_name", "last_name",
+				"email", 'age', 
+				'language', 'home_planet',
+			)}
+		),
+	)
+
+	# fields displayed during modification
+	fieldsets = (
+		( 
+			None, { "fields": (
+				"username", "password",
+				'security_question', 'security_answer',
+			)},
+		),
+		( 
+			_("Personal info"), 
+			{ "fields": (
+				"first_name", "last_name", 
+				"email", 'age', 
+				'language', 'home_planet',
+			)},
+		),
+        (
+            _("Permissions"),
+            { "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+			)},
+        ),
+        ( 
+			_("Important dates"), 
+			{ "fields": (
+				"last_login", "date_joined"
+			)},
+		),
+	)
