0aad0eb0dd59b7bd006ed1e3456d0d209353d3f3
Starz <elitesorenea1@gmail.com>
2024-11-12 12:19:58 -0700

    Stellar App

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-FORCE_SCRIPT_NAME = '/' + os.environ.get('SITE_NAME', '')
+FORCE_SCRIPT_NAME = '/' + os.environ.get('SITE_NAME', '') if os.environ.get('SITE_NAME', '') != '' else ''
 
 
 # Quick-start development settings - unsuitable for production
