d88a550c1332ebb14703f9bbd5c0ba756f50e97a
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2024-11-17 20:34:12 -0700

    make sure the user get's linked to their profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,9 @@
 import logging
+from typing import Any
 
 from django.conf import settings
+from django.forms import BaseModelForm, Form
+from django.http import HttpRequest, HttpResponse
 from django.http.response import HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin, AccessMixin
@@ -9,21 +12,62 @@ from django.views.generic import CreateView, DetailView, UpdateView, ListView
 
 from .forms import SpaceTravelerProfileForm
 from .models import SpaceTravelerProfile
+from django.contrib.auth.models import User
 
 
-class SpaceTravelerProfileCreateView(LoginRequiredMixin, CreateView):
+class SpaceTravelerProfileCreateView(LoginRequiredMixin, UserPassesTestMixin, AccessMixin, CreateView):
 	form_class = SpaceTravelerProfileForm
 	login_url = settings.LOGIN_URL
 	# redirect_field_name = 'index'
 	success_url = reverse_lazy('home')
 	template_name = 'profiles/create.html'
 
+
+	def test_func(self, *args, **kwargs) -> bool:
+		"""
+		Ensure they *don't* already have a profile. If they do, they will be redirected.
+		"""
+		has_profile = None
+		if self.request.user.is_authenticated:
+			try:
+				SpaceTravelerProfile.objects.get(real_account=self.request.user)
+				has_profile = True
+			except SpaceTravelerProfile.DoesNotExist:
+				has_profile = False
+		else:
+			has_profile = False
+
+		return not has_profile
+		
+		
+	def handle_no_permission(self):
+		"""
+		When the currently signed-in user has a profile already, just redirect them to profiles:update instead.
+		"""
+		return redirect('profiles:update', pk=SpaceTravelerProfile.objects.get(real_account=self.request.user).pk)
+
+
+	def get_form(self, form_class: BaseModelForm | None = None) -> BaseModelForm:
+		"""
+		Set the real_account field (hidden) to link the profile to the actual user account that is currently signed in.
+		"""
+		form = super().get_form(form_class) #type:ignore
+		form.initial['real_account'] = User.objects.get(pk=self.request.user.pk)
+		return form
+
+
 	def form_valid(self, form):
-		obj = form.save(commit=False)
-		# form.real_account = SpaceTravelerProfile.objects.get(real_account=self.request.user)
-		form.initial['real_account'] = self.request.user
+		"""
+		Quadruple-check that the real_account field (hidden) is set to the currently signed-in user, then save the form.
+		"""
+		obj: SpaceTravelerProfile = form.save(commit=False)
+		# form.real_account = User.objects.get(pk=self.request.user.pk)
+		# form.data
+		# form.initial['real_account'] = self.request.user
+		obj.real_account = User.objects.get(pk=self.request.user.pk)
+		# form.real_account = User.objects.get(pk=self.request.user.pk)
 		# logging.info(self.request.user)
-		form.save()
+		obj.save()
 		# return HttpResponseRedirect(self.get_success_url())
 		return super().form_valid(form)
 
