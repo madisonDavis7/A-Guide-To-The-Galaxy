17c529a7fa40ddfb1e275f0423fa2afaf0b7ffd4
madisonDavis7 <penny-davis-12@outlook.com>
2024-11-25 00:31:10 -0700

    added urls/views for each planet. put planetary home within app not project file

--- planetary/views.py
+++ planetary/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+def planetary(request):
+    return render(request, 'planetary/planetary.html')
+
+def mercury_page(request):
+    return render(request, 'planetary/mercury_page.html')
+
+def mars_page(request):
+    return render(request, 'planetary/mars_page.html')
+
+def neptune_page(request):
+    return render(request, 'planetary/neptune_page.html')
+
+def pluto_page(request):
+    return render(request, 'planetary/pluto_page.html')
+
+def kepler_page(request):
+    return render(request, 'planetary/depler_page.html')
+
+def sedna_page(request):
+    return render(request, 'planetary/sedna.html')
+
