1d5d66a0fb42f2b09d9cac19df19fde8382b6218
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2024-12-02 14:10:56 -0700

    kinda what you need to do

--- planetary/models.py
+++ planetary/models.py
@@ -3,6 +3,8 @@ from star_ratings.models import Rating
 
 
 class PlanetaryTour(models.Model):
+    location_and_orbit = models.TextField()
+    about_planet = models.TextField()
     name = models.CharField(max_length=100) #name of tour
     length = models.PositiveIntegerField() #length in days
     num_people = models.PositiveIntegerField() #num of people who can go
