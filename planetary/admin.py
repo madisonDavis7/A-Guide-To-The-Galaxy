ef00897f07e5671b4eb8fbe6bf11a59cc2af5068
madisonDavis7 <penny-davis-12@outlook.com>
2024-12-12 16:53:07 -0700

    was trying some stuff out but didnt change anything

--- planetary/admin.py
+++ planetary/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import PlanetaryTour
 
 class PlanetaryTourAdmin(admin.ModelAdmin):
-    model = PlanetaryTourlist_display = (
+    model = PlanetaryTour
+    list_display = (
         'location_and_orbit',
         'about_planet', 
         'name', 
@@ -14,4 +15,4 @@ class PlanetaryTourAdmin(admin.ModelAdmin):
         'image4', 
         'link',
     )
-admin.site.register(PlanetaryTour)
+admin.site.register(PlanetaryTour, PlanetaryTourAdmin)
